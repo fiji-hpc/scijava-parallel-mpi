0b8cf17e825c865d02b69ce983490fb510f2d523
Daniel Trnka <daniel.trnka@gmail.com>
2020-01-21 22:00:37 +0100

    Cell broadcasting

--- src/main/java/com/mycompany/imagej/ops/filter/convolve/MPIConvolution.java
+++ src/main/java/com/mycompany/imagej/ops/filter/convolve/MPIConvolution.java
@@ -2,22 +2,31 @@ package com.mycompany.imagej.ops.filter.convolve;
 
 import com.mycompany.imagej.MPIUtils;
 import com.mycompany.imagej.Utils;
+import io.scif.img.cell.SCIFIOCellImg;
 import net.imagej.ops.Contingent;
 import net.imagej.ops.Ops;
 import net.imagej.ops.special.computer.AbstractUnaryComputerOp;
 import net.imglib2.RandomAccessible;
 import net.imglib2.RandomAccessibleInterval;
+import net.imglib2.converter.AbstractConvertedIterableInterval;
 import net.imglib2.img.array.ArrayImg;
 import net.imglib2.img.basictypeaccess.array.ByteArray;
+import net.imglib2.img.basictypeaccess.volatiles.array.DirtyVolatileByteArray;
+import net.imglib2.img.cell.Cell;
+import net.imglib2.img.cell.CellImg;
+import net.imglib2.img.cell.LazyCellImg;
+import net.imglib2.img.list.ListImg;
 import net.imglib2.type.numeric.RealType;
 import net.imglib2.util.Intervals;
 import net.imglib2.view.IterableRandomAccessibleInterval;
+import net.imglib2.view.Views;
 import org.scijava.Priority;
 import org.scijava.plugin.Parameter;
 import org.scijava.plugin.Plugin;
 
 import java.nio.ByteBuffer;
 import java.util.List;
+import java.util.concurrent.ExecutionException;
 
 @Plugin(type = Ops.Filter.Convolve.class, priority = Priority.HIGH + 2)
 public class MPIConvolution<I extends RealType<I>, K extends RealType<K>, O extends RealType<O>>
@@ -52,22 +61,79 @@ public class MPIConvolution<I extends RealType<I>, K extends RealType<K>, O exte
             b.setReal(value);
         }
 
+        if(output instanceof ArrayImg) {
+            ArrayImg img = (ArrayImg) output;
+            int off = 0;
+            for(int i = 0; i < blocks.size(); i++) {
+                RandomAccessibleInterval<O> block = blocks.get(i);
+                byte[] data = ((ByteArray) img.update(null)).getCurrentStorageArray();
+                int length = (int) Intervals.numElements(block);
 
-        ArrayImg img = (ArrayImg) output;
-        int off = 0;
-        for(int i = 0; i < blocks.size(); i++) {
-            RandomAccessibleInterval<O> block = blocks.get(i);
-            byte[] data = ((ByteArray) img.update(null)).getCurrentStorageArray();
-            int length = (int) Intervals.numElements(block);
-
-            int ret = MPIUtils.MPILibrary.INSTANCE.MPI_Bcast(
-                    ByteBuffer.wrap(data, off, length).slice(),
-                    length, MPIUtils.MPI_BYTE, i, MPIUtils.MPI_COMM_WORLD);
-            if(ret != 0) {
-                throw new RuntimeException("mpi failed");
+                int ret = MPIUtils.MPILibrary.INSTANCE.MPI_Bcast(
+                        ByteBuffer.wrap(data, off, length).slice(),
+                        length, MPIUtils.MPI_BYTE, i, MPIUtils.MPI_COMM_WORLD);
+                if(ret != 0) {
+                    throw new RuntimeException("mpi failed");
+                }
+                off += length;
             }
-            off += length;
+        } else if(output instanceof SCIFIOCellImg) {
+            SCIFIOCellImg img = (SCIFIOCellImg) output;
+            Utils.rootPrint(img.getCellGrid());
+
+            int cols = (int) img.dimension(0);
+
+            int cell_off = 0;
+            Cell c = get(img, 0);
+            long lastCell = 0;
+            for(int i = 0; i < blocks.size(); i++) {
+                RandomAccessibleInterval<O> block = blocks.get(i);
+                int block_rows = (int) block.dimension(1);
+
+                int sent = 0;
+                while(sent < block_rows) {
+                    Utils.rootPrint(block_rows);
+                    int cell_rows = img.getCellGrid().getCellDimension(1, lastCell);
+                    int length = Math.min(block_rows - sent, cell_rows) - cell_off;
+                  //  Utils.print(block + " cell=" + lastCell + " cell_ofset=" + cell_off + " length=" + length + "; node=" + i);
+
+                    byte[] data = ((DirtyVolatileByteArray) c.getData()).getCurrentStorageArray();
+
+                    int ret = MPIUtils.MPILibrary.INSTANCE.MPI_Bcast(
+                            ByteBuffer.wrap(data, cell_off * cols, cols * length).slice(),
+                            cols * length, MPIUtils.MPI_BYTE, i, MPIUtils.MPI_COMM_WORLD);
+                    if(ret != 0) {
+                        throw new RuntimeException("mpi failed");
+                    }
+                    ((DirtyVolatileByteArray) c.getData()).setDirty();
+
+                    cell_off += length;
+                    if(cell_off > cell_rows) {
+                        System.out.println("UNEXPECTED");
+//                        throw new RuntimeException("UNEXPECTED");
+                    }
+                    if(cell_off == cell_rows) {
+                        lastCell++;
+                        cell_off = 0;
+
+                        c = get(img, lastCell);
+                    }
+
+                    sent += length;
+                }
+            }
+
+            } else {
+            throw new RuntimeException("Unsupported!");
         }
     }
 
+    public Cell get(SCIFIOCellImg img, long index) {
+        try {
+            return (Cell) img.getCache().get(index);
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 }
