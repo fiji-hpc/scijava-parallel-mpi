e8dc097a17b0982305d1720c4566d30b4653c212
Daniel Trnka <daniel.trnka@gmail.com>
2020-01-26 13:28:01 +0100

    env.sh: generate notebook reports, thumbnails, checksums

--- env.sh
+++ env.sh
@@ -4,7 +4,11 @@ export ROUNDS=${ROUNDS:-1}
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 export CLASSPATH=$(< "$DIR"/classpath)
 
-run() {
+benchenv() {
+    env | grep ^B_
+}
+
+benchrun() {
   op=$1
 	dataset=$2
 
@@ -18,16 +22,40 @@ run() {
 		mkdir -p "$DIR/run/outputs/$date"
 		cd "$DIR/run/outputs/$date"
     echo "$op $dataset" > info
+    benchenv > env
 
 		(cd ..; rm -f latest; ln -s "$date" latest)
-		for nodes in $(seq "$MAX_NODES" -1 1); do
+		for nodes in $(seq "$B_MAX_NODES" -1 1); do
 			output="$nodes/$dataset"
 			output_dir="$(dirname "$output")"
 			mkdir -p "$output_dir"
 
 			P="-agentpath:$HOME/profiler/libasyncProfiler.so=start,alluser,file=$output_dir/profile.%p.svg"
-			cmd="mpirun --oversubscribe -np $nodes java $P com.mycompany.imagej.Main $op  ../../datasets/$dataset $nodes/result.tiff"
+			cmd="mpirun --bind-to none --oversubscribe -np $nodes java $P com.mycompany.imagej.Main $op  ../../datasets/$dataset $nodes/result.tiff"
 			(echo $cmd; $cmd < /dev/null) |& tee -a out
 		done
 	)
+}
+
+benchpostprocess() (
+  cd "$DIR/run/outputs"
+  for result in */; do
+    if [ ! -f "$result/checksums" ]; then
+      find "$result" -name "result.tiff" -exec md5sum {} \; > "$result/checksums"
+    fi
+
+    find "$result" -name "result.tiff" | while read -r result_image; do
+      thumbnail_path="${result_image%/*}/result.thumbnail.png"
+      if [ ! -f "$thumbnail_path" ]; then
+        convert "$result_image" -resize 500x "$thumbnail_path"
+      fi
+    done
+  done
+)
+
+benchreports() {
+  path="$DIR/run/outputs/"
+  find "$path" -maxdepth 1 ! -path "$path" -type d | while read -r report_dir; do
+    B_RESULT_DIR="$report_dir" jupyter nbconvert --execute --to html --stdout "$DIR/report.ipynb" > "$report_dir/report.html"
+  done
 }
