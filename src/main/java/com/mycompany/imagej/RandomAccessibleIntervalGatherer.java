4e742c23e79d7d64c2ef28bd13deb7e2a2420028
Daniel Trnka <daniel.trnka@gmail.com>
2020-02-03 11:36:24 +0100

    PlanarGatherer

--- src/main/java/com/mycompany/imagej/RandomAccessibleIntervalGatherer.java
+++ src/main/java/com/mycompany/imagej/RandomAccessibleIntervalGatherer.java
@@ -4,10 +4,14 @@ import net.imagej.Dataset;
 import net.imagej.ImgPlus;
 import net.imglib2.Cursor;
 import net.imglib2.RandomAccessibleInterval;
+import net.imglib2.img.basictypeaccess.array.ArrayDataAccess;
 import net.imglib2.img.planar.PlanarImg;
 import net.imglib2.type.numeric.RealType;
 import net.imglib2.type.numeric.integer.ByteType;
 import net.imglib2.view.IntervalView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static com.mycompany.imagej.Measure.measureCatch;
 
@@ -28,13 +32,12 @@ public class RandomAccessibleIntervalGatherer {
 
       RandomAccessibleInterval<O> finalOutput = output;
       measureCatch("gather", () -> {
-         if(System.getenv("B_GATHER_GENERIC") != null || true) {
+         if(System.getenv("B_GATHER_GENERIC") != null) {
             Utils.rootPrint("Gather: gatherGeneric (overridden)");
             gatherGeneric(chunk);
          } else if(finalOutput instanceof PlanarImg) {
             Utils.rootPrint("Gather: gatherPlanar");
-            throw new RuntimeException("not implemented!");
-//            gatherPlanar(blocks, (PlanarImg) finalOutput);
+            gatherPlanar(chunk, (PlanarImg) finalOutput);
          } else {
             Utils.rootPrint("Gather: gatherGeneric");
             gatherGeneric(chunk);
@@ -42,7 +45,49 @@ public class RandomAccessibleIntervalGatherer {
       });
    }
 
-   private static <O extends RealType<O>> void gatherGeneric(Chunk<O> chunks) {
+   private static <O extends RealType<O>> void gatherPlanar(Chunk<O> chunks, PlanarImg img) {
+      NonBlockingBroadcast transfer = new NonBlockingBroadcast();
+      int currentRoot = 0;
+
+      ArrayDataAccess<?> c = img.getPlane(0);
+      int planeOffset = 0;
+      int lastPlane = 0;
+
+      for(Chunk<O> chunk: chunks) {
+            List<NonBlockingBroadcast.Block> transferBlocks = new ArrayList<>();
+
+            long sent = 0;
+            long totalLen = chunk.getLen();
+            while(sent < totalLen) {
+               int length = (int) Math.min(totalLen - sent, c.getArrayLength() - planeOffset);
+               transferBlocks.add(new NonBlockingBroadcast.Block(c.getCurrentStorageArray(), planeOffset, length));
+
+               planeOffset += length;
+               if(planeOffset > c.getArrayLength()) {
+                  throw new RuntimeException("UNEXPECTED");
+               }
+               if(planeOffset == c.getArrayLength()) {
+                  lastPlane++;
+                  planeOffset = 0;
+
+                  if(lastPlane < img.numSlices()) {
+                     c = img.getPlane((int) lastPlane);
+                  }
+               }
+
+               sent += length;
+            }
+
+            transfer.requestTransfer(currentRoot++, transferBlocks);
+         }
+         if(lastPlane != img.numSlices()) {
+            throw new RuntimeException("Unexpected");
+         }
+
+         transfer.waitForTransfer();
+      }
+
+      private static <O extends RealType<O>> void gatherGeneric(Chunk<O> chunks) {
       NonBlockingBroadcast b = new NonBlockingBroadcast();
       byte[][] storages = new byte[MPIUtils.getSize()][];
       int c = 0;
