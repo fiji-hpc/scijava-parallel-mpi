c9b016413055af8bb2d95b4e4fcf175e692f5bb2
Daniel Trnka <daniel.trnka@gmail.com>
2020-02-07 10:30:50 +0100

    PlanarGatherer: support packed types - BitType

--- src/main/java/com/mycompany/imagej/gatherer/PlanarGatherer.java
+++ src/main/java/com/mycompany/imagej/gatherer/PlanarGatherer.java
@@ -2,8 +2,10 @@ package com.mycompany.imagej.gatherer;
 
 import com.mycompany.imagej.NonBlockingBroadcast;
 import com.mycompany.imagej.chunk.Chunk;
+import net.imglib2.Cursor;
 import net.imglib2.img.basictypeaccess.array.ArrayDataAccess;
 import net.imglib2.img.planar.PlanarImg;
+import net.imglib2.type.NativeType;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -25,19 +27,19 @@ public class PlanarGatherer<O> extends AbstractGatherer<O> {
         int planeOffset = 0;
         int lastPlane = 0;
 
+        NativeType<?> type = getType(chunks);
+
         for(Chunk<O> chunk: chunks.allChunks()) {
             List<NonBlockingBroadcast.Block> transferBlocks = new ArrayList<>();
 
             long sent = 0;
-            long totalLen = chunk.getLen();
+            long totalLen = chunk.getLen() / type.getEntitiesPerPixel().getDenominator();
             while(sent < totalLen) {
                 int length = (int) Math.min(totalLen - sent, c.getArrayLength() - planeOffset);
                 transferBlocks.add(new NonBlockingBroadcast.Block(c.getCurrentStorageArray(), planeOffset, length));
 
                 planeOffset += length;
-                if(planeOffset > c.getArrayLength()) {
-                    throw new RuntimeException("UNEXPECTED");
-                }
+
                 if(planeOffset == c.getArrayLength()) {
                     lastPlane++;
                     planeOffset = 0;
@@ -52,12 +54,15 @@ public class PlanarGatherer<O> extends AbstractGatherer<O> {
 
             transfer.requestTransfer(currentRoot++, transferBlocks);
         }
-        if(lastPlane != img.numSlices()) {
-            throw new RuntimeException("Unexpected");
-        }
 
         transfer.waitForTransfer();
 
         return true;
+    }
+
+    private NativeType getType(Chunk<O> chunks) {
+        Cursor c = chunks.cursor();
+        c.fwd();
+        return (NativeType) c.get();
     }
 }
