442dcbd2545e82e3de660b0c0f08e79eb6274ce4
Daniel Trnka <daniel.trnka@gmail.com>
2020-02-14 21:34:52 +0100

    build: fiji plugin

--- build.sh
+++ build.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
+set -e
+
 cd "$( dirname "${BASH_SOURCE[0]}" )"
 
 mvn compile
 mvn dependency:build-classpath -Dmdep.outputFile=classpath
 sed -i "1s;^;$PWD/target/classes/:;" classpath
+
+mvn -Dscijava.app.directory="$HOME/Fiji.app"
+echo OK
