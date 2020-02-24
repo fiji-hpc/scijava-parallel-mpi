155477d503a26a88796bf509945a390afe6e43ab
Daniel Trnka <daniel.trnka@gmail.com>
2020-02-24 14:38:19 +0100

    native minfilter: split thread block manually, remove some divides

--- native/minfilter.c
+++ native/minfilter.c
@@ -1,26 +1,35 @@
+#include <omp.h>
 #include "utils.h"
 #include "split.h"
 
 #define at(x, y, z) input[(z) * dims[1] * dims[0] + (y) * dims[0] + (x)]
 
 void minfilter(uint8_t *output, uint8_t *input, int64_t *dims, int n_dims, int neighSize) {
-    struct split split;
-    split_init(&split, output, numElements(dims, n_dims));
+  struct split split;
+  split_init(&split, output, numElements(dims, n_dims));
 
-    uint64_t begin, end;
-    split_myblock_pos(&split, &begin, &end);
-    #pragma omp parallel for
+  uint64_t node_begin, node_end;
+  split_myblock_pos(&split, &node_begin, &node_end);
+
+  #pragma omp parallel
+  {
+    int num_threads = omp_get_num_threads();
+    uint64_t node_elements = node_end - node_begin;
+    uint64_t per_thread = (node_elements + num_threads) / num_threads;
+    uint64_t begin = node_begin + per_thread * omp_get_thread_num();
+    uint64_t end = MIN(begin + per_thread, node_end);
+
+    int64_t Z = begin / (dims[0] * dims[1]);
+    int64_t Y = (begin % (dims[0] * dims[1])) / dims[0];
+    int64_t X = (begin % (dims[0] * dims[1])) % dims[0];
+
     for(uint64_t cur = begin; cur < end; cur++) {
-      int64_t Z = cur / (dims[0] * dims[1]);
-      int64_t Y = (cur % (dims[0] * dims[1])) / dims[0];
-      int64_t X = (cur % (dims[0] * dims[1])) % dims[0];
-
       uint8_t min = 255;
       for(int64_t x = X - neighSize; x <= X + neighSize; x++) {
         for(int64_t y = Y - neighSize; y <= Y + neighSize; y++) {
           for(int64_t z = Z - neighSize; z <= Z + neighSize; z++) {
             if(x < 0 || y < 0 || z < 0 || x >= dims[0] || y >= dims[1] || z >= dims[2]) {
-                continue;
+              continue;
             }
 
             uint8_t val = at(x, y, z);
@@ -31,55 +40,19 @@ void minfilter(uint8_t *output, uint8_t *input, int64_t *dims, int n_dims, int n
         }
       }
       output[cur] = min;
-    }
 
-    split_sync(&split);
-    split_free(&split);
+      X++;
+      if(X >= dims[0]) {
+        X = 0;
+        Y++;
+        if(Y >= dims[1]) {
+          Y = 0;
+          Z++;
+        }
+      }
+    }
+  }
+
+  split_sync(&split);
+  split_free(&split);
 }
-
-
-/*
-int mpirank(uint8_t *output, uint8_t *input, int64_t *dims, int n_dims) {
-
-    int rankSize = world_size();
-    int myRank = world_rank();
-
-    uint64_t totalElements = numElements(dims, n_dims);
-    uint64_t perNode = (totalElements + rankSize) / rankSize;
-
-    uint64_t myOffset = perNode * myRank;
-    uint64_t myEnd = MIN(myOffset + perNode, totalElements);
-
-    uint8_t *cur = output + myOffset;
-    uint8_t *end = output + myEnd - 1;
-
-
-    uint8_t color_offset = 10;
-    uint8_t val = color_offset + (255 - color_offset) * myRank / rankSize;
-    printf("%d %d %d\n", myOffset, myEnd, val);
-    while(cur != end) {
-        *cur = val;
-        cur++;
-    }
-
-    int recvcounts[rankSize];
-    int displs[rankSize];
-    for(int i = 0; i < rankSize; i++) {
-        recvcounts[i] = perNode;
-        displs[i] = i * perNode;
-    }
-    recvcounts[rankSize - 1] = MIN(displs[rankSize - 1] + perNode, totalElements) - displs[rankSize - 1];
-
-    MPI_CHECK(MPI_Allgatherv(
-        output + myOffset,
-        myEnd - myOffset,
-        MPI_BYTE,
-        output,
-        recvcounts,
-        displs,
-        MPI_BYTE,
-        MPI_COMM_WORLD
-    ));
-}
-
-*/
