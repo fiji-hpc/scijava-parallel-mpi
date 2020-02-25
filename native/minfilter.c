12596b6a454e0b1d750a47ced8d500db2ddf711c
Daniel Trnka <daniel.trnka@gmail.com>
2020-02-25 12:26:38 +0100

    native: support 5 dimensions use -O3 and -march=native

--- native/minfilter.c
+++ native/minfilter.c
@@ -3,7 +3,7 @@
 #include "split.h"
 
 #define at(x, y, z) input[(z) * dims[1] * dims[0] + (y) * dims[0] + (x)]
-
+#define unlikely(x)     __builtin_expect(!!(x), 0)
 
 
 
@@ -24,26 +24,59 @@ void minfilter(uint8_t *output, uint8_t *input, int64_t *dims, int n_dims, int n
 
     int64_t pos[n_dims];
     index_to_pos(begin, pos, dims, n_dims);
-    printf("%d %d %d %d\n", pos[0], pos[1], pos[2], n_dims);
+    
+    if(n_dims == 3) {
+      for(uint64_t cur = begin; cur < end; cur++) {
+        uint8_t min = 255;
+        for(int64_t z = pos[2] - neighSize; z <= pos[2] + neighSize; z++) {
+          if(z < 0 || z >= dims[2]) continue;
+          for(int64_t y = pos[1] - neighSize; y <= pos[1] + neighSize; y++) {
+            if(y < 0 || y >= dims[1]) continue;
+            for(int64_t x = pos[0] - neighSize; x <= pos[0] + neighSize; x++) {
+              if(x < 0 || x >= dims[0]) continue;
 
-    for(uint64_t cur = begin; cur < end; cur++) {
-      uint8_t min = 255;
-      for(int64_t x = pos[0] - neighSize; x <= pos[0] + neighSize; x++) {
-        if(x < 0 || x >= dims[0]) continue;
-        for(int64_t y = pos[1] - neighSize; y <= pos[1] + neighSize; y++) {
-          if(y < 0 || y >= dims[1]) continue;
-          for(int64_t z = pos[2] - neighSize; z <= pos[2] + neighSize; z++) {
-            if(z < 0 || z >= dims[2]) continue;
-
-            uint8_t val = at(x, y, z);
-            if(val < min) {
-              min = val;
+              uint8_t val = at(x, y, z);
+              if(val < min) {
+                min = val;
+              }
             }
           }
         }
+        output[cur] = min;
+        fwd(pos, dims, n_dims);
       }
-      output[cur] = min;
-      fwd(pos, dims, n_dims);
+    } else if(n_dims == 5) {
+      for(uint64_t cur = begin; cur < end; cur++) {
+        uint8_t min = 255;
+        for(int64_t t = pos[4] - neighSize; t <= pos[4] + neighSize; t++) {
+          if(unlikely(t < 0 || t >= dims[4])) continue;
+          for(int64_t c = pos[3] - neighSize; c <= pos[3] + neighSize; c++) {
+            if(unlikely(c < 0 || c >= dims[3])) continue;
+            for(int64_t z = pos[2] - neighSize; z <= pos[2] + neighSize; z++) {
+              if(unlikely(z < 0 || z >= dims[2])) continue;
+              for(int64_t y = pos[1] - neighSize; y <= pos[1] + neighSize; y++) {
+                if(unlikely(y < 0 || y >= dims[1])) continue;
+                for(int64_t x = pos[0] - neighSize; x <= pos[0] + neighSize; x++) {
+                  if(unlikely(x < 0 || x >= dims[0])) continue;
+
+                  uint64_t idx = t * dims[3] * dims[2] * dims[1] * dims[0]
+                               + c * dims[2] * dims[1] * dims[0]
+                               + z * dims[1] * dims[0]
+                               + y * dims[0]
+                               + x;
+
+                  uint8_t val = input[idx];
+                  if(unlikely(val < min)) {
+                    min = val;
+                  }
+                }
+              }
+            }
+          }
+        }
+        output[cur] = min;
+        fwd(pos, dims, n_dims);
+      }
     }
   }
 
