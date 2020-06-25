e5c50e944fd2d91f0e484ee760e10763a97fad5e
Daniel Trnka <daniel.trnka@gmail.com>
2020-06-25 10:40:46 +0200

    Added canny script

---
+++ scripts/canny.py
@@ -0,0 +1,33 @@
+#@ OpService ops
+#@ SCIFIO scifio
+#@ String input_path
+#@ String output_path
+#@ UIService ui
+#@ DatasetService datasets
+
+from cz.it4i.scijava.mpi import Measure
+from net.imglib2.type.numeric.real import DoubleType
+from net.imglib2.view import Views
+
+rgb = scifio.datasetIO().open(input_path)
+
+grayscale = ops.run("convert.rgb2grayscale", rgb)
+grayscale = ops.convert().float64(grayscale)
+rgb = None
+
+gauss_kernel = ops.create().kernel([
+	[1 / 256.0, 4 / 256.0, 6 / 256.0, 4 / 256.0, 1 / 256.0],
+	[4 / 256.0, 14 / 256.0, 24 / 256.0, 14 / 256.0, 4 / 256.0],
+	[6 / 256.0, 24 / 256.0, 36 / 256.0, 24 / 256.0, 6 / 256.0],
+	[4 / 256.0, 14 / 256.0, 24 / 256.0, 14 / 256.0, 4 / 256.0],
+	[1 / 256.0, 4 / 256.0, 6 / 256.0, 4 / 256.0, 1 / 256.0]
+], DoubleType())
+
+without_noise = ops.create().img(grayscale)
+ops.filter().convolve(without_noise, Views.extendMirrorSingle(grayscale), gauss_kernel)
+
+edges = ops.run("edgeDetector", without_noise)
+scifio.datasetIO().save(datasets.create(ops.convert().uint8(ops.math().multiply(edges, 255))), output_path)
+print("OK")
+
+#ui.show(edges)
