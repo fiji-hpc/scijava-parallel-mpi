b5e35e2889753d38670101d7ccd8b7de67e88bc7
Daniel Trnka <daniel.trnka@gmail.com>
2020-10-07 18:01:38 +0200

    pipeline_preibisch: parametrized sigma, low and high threshold

--- scripts/pipeline_preibisch.py
+++ scripts/pipeline_preibisch.py
@@ -4,6 +4,11 @@
 #@ String output_path
 #@ UIService ui
 #@ DatasetService datasets
+#@ float (value=3) sigma
+#@ float (value=0.0005) low_threshold
+#@ float (value=0.001) high_threshold
+
+print(sigma, low_threshold, high_threshold)
 
 from cz.it4i.scijava.mpi import Measure
 from net.imglib2.type.numeric.real import FloatType
@@ -18,7 +23,7 @@ minMax = ops.stats().minMax(input_dataset)
 input_dataset = ops.image().normalize(preprocess, input_dataset, minMax.getA(), minMax.getB(), FloatType(0), FloatType(1.0))
 #ui.show(preprocess)
 
-gauss_kernel =  ops.create().kernelGauss([3, 3])
+gauss_kernel =  ops.create().kernelGauss([sigma, sigma])
 print(gauss_kernel)
 
 without_noise = ops.create().img(input_dataset)
@@ -26,7 +31,7 @@ ops.filter().convolve(without_noise, Views.extendMirrorSingle(input_dataset), ga
 input_dataset = None
 #ui.show(without_noise)
 
-edges = ops.run("edgeDetector", without_noise, 0.0008, 0.001)
+edges = ops.run("edgeDetector", without_noise, low_threshold, high_threshold)
 #ui.show(edges)
 scifio.datasetIO().save(datasets.create(ops.convert().uint8(ops.math().multiply(edges, FloatType(255)))), output_path)
 
