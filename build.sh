823c38b1244b8e2fb51a197873efc6827e2a4f0b
Dimitrios Stefanos Velissariou <velissarious@gmail.com>
2021-12-13 12:23:19 +0100

    upgrade build script
    The script can take Fiji's path as a parameter, if none is provided it will try to install in a Fiji in the home directory as before.

--- build.sh
+++ build.sh
@@ -1,4 +1,10 @@
-#!/bin/bash
+#!/usr/bin/env bash
+#
+# This script builds required libraries and copies into 
+#   Fiji installation directory.
+#
+#Usage: do-release <Directory with Fiji installation>
+#
 set -e
 
 cd "$( dirname "${BASH_SOURCE[0]}" )"
@@ -7,5 +13,11 @@ mvn compile
 mvn dependency:build-classpath -Dmdep.outputFile=classpath
 sed -i "1s;^;$PWD/target/classes/:;" classpath
 
-mvn -Dscijava.app.directory="$HOME/Fiji.app"
-echo OK
+if [ $# -eq 0 ]; then
+    fiji="$HOME/Fiji.app"
+else
+	fiji=$1
+fi
+
+mvn -Dscijava.app.directory="$fiji"
+echo "installed in $fiji"
